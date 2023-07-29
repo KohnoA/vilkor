a4eacd28876ad3fb88a6341e1d67feae58572aff
KohnoA <san.kohno@yandex.ru>
2023-07-30 01:50:02 +0300

    fix: background-attachment

--- src/components/UI/Background/Background.tsx
+++ src/components/UI/Background/Background.tsx
@@ -2,7 +2,7 @@ import { StaticImageData } from 'next/image';
 import styles from './Background.module.scss';
 // import defaultTexture from 'public/images/white-texture.jpg';
 import defaultTexture from 'public/images/white-texture-optim.jpg';
-import { memo, useEffect, useRef } from 'react';
+import { memo } from 'react';
 
 interface BackgroundProps {
   children: React.ReactNode;
@@ -14,35 +14,16 @@ interface BackgroundProps {
 const DEFAULT_OPACITY = '0.6';
 
 function Background({ image, opacity, fixed = true, children }: BackgroundProps) {
-  // const bgRef = useRef<null | HTMLDivElement>(null);
-
-  // useEffect(() => {
-  //   const test = () => {
-  //     const newImage = new Image();
-
-  //     newImage.src = image?.src ?? defaultTexture.src;
-  //     newImage.onload = () => {
-  //       const refCur = bgRef.current;
-
-  //       if (refCur) {
-  //         refCur.style.backgroundImage = `url(${newImage.src})`
-  //       }
-  //     };
-  //   };
-
-  //   test();
-  // }, []);
-
   return (
     <section className={styles.container}>
       <div
         // ref={bgRef}
-        className={styles.bg}
+        className={`${styles.bg} ${fixed ? styles.bg_fixed : ''}`}
         style={{
           backgroundImage: `url(${image?.src ?? defaultTexture.src})`,
           // backgroundImage: `url(${bgImage})`,
           opacity: opacity ? String(opacity / 100) : DEFAULT_OPACITY,
-          backgroundAttachment: fixed ? 'fixed' : 'none',
+          // backgroundAttachment: fixed ? 'fixed' : 'none',
         }}
       ></div>
       <div className={styles.children}>{children}</div>
