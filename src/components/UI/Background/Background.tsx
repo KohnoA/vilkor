1486515ec58a1196cbfb92ed78f51e0ea0f04891
KohnoA <san.kohno@yandex.ru>
2023-07-29 20:32:05 +0300

    fix: test clip-path bug

--- src/components/UI/Background/Background.tsx
+++ src/components/UI/Background/Background.tsx
@@ -14,36 +14,24 @@ const DEFAULT_OPACITY = '0.6';
 
 function Background({ image, opacity, fixed = true, children }: BackgroundProps) {
   return (
-    <section style={{ position: 'relative' }}>
+    <section className={styles.container}>
       <div
-        className={styles.bg}
+        className={styles.imageWrapper}
         style={{
-          backgroundImage: `url(${image?.src ?? defaultTexture.src})`,
-          backgroundAttachment: fixed ? 'fixed' : 'none',
           opacity: opacity ? String(opacity / 100) : DEFAULT_OPACITY,
+          position: fixed ? 'fixed' : 'absolute',
         }}
-      ></div>
-      {/* {children} */}
+      >
+        <Image
+          className={styles.image}
+          src={image ?? defaultTexture}
+          fill
+          sizes="1440px"
+          alt="Задний фон"
+        />
+      </div>
       <div className={styles.children}>{children}</div>
     </section>
-    // <section className={styles.container}>
-    //   <div
-    //     className={styles.imageWrapper}
-    //     style={{
-    //       opacity: opacity ? String(opacity / 100) : DEFAULT_OPACITY,
-    //       position: fixed ? 'fixed' : 'absolute',
-    //     }}
-    //   >
-    //     <Image
-    //       className={styles.image}
-    //       src={image ?? defaultTexture}
-    //       fill
-    //       sizes="1440px"
-    //       alt="Задний фон"
-    //     />
-    //   </div>
-    //   <div className={styles.children}>{children}</div>
-    // </section>
   );
 }
 
