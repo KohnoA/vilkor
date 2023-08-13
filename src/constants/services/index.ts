8bcdf646adae0d2dfc7dea3271c71b2c6ce290e4
KohnoA <san.kohno@yandex.ru>
2023-08-13 16:42:07 +0300

    feat: separate services

---
+++ src/constants/services/index.ts
@@ -0,0 +1,22 @@
+import { ServicesDataTypeGroup } from '@/types';
+import { DISMANTLING } from './dismantling';
+import { CONCRETE } from './concrete';
+import { ROOFING } from './roofing';
+import { FINISHING } from './finishing';
+import { FACING } from './facing';
+import { PLUMBING } from './plumbing';
+import { WIRING } from './wiring';
+import { FLOORING } from './flooring';
+import { LANDSCAPE } from './landscape';
+
+export const SERVICES: ServicesDataTypeGroup[] = [
+  DISMANTLING,
+  CONCRETE,
+  ROOFING,
+  FINISHING,
+  FACING,
+  PLUMBING,
+  WIRING,
+  FLOORING,
+  LANDSCAPE,
+];
