41609d12e608f6287210d84fafd209d5704ff62a
KohnoA <san.kohno@yandex.ru>
2024-04-26 19:12:32 +0300

    fix: update services info

--- src/constants/services/index.ts
+++ src/constants/services/index.ts
@@ -1,22 +1,6 @@
 import { ServicesDataTypeGroup } from '@/types';
 import { DISMANTLING } from './dismantling';
-import { CONCRETE } from './concrete';
-import { ROOFING } from './roofing';
-import { FINISHING } from './finishing';
-import { FACING } from './facing';
-import { PLUMBING } from './plumbing';
-import { WIRING } from './wiring';
 import { FLOORING } from './flooring';
 import { LANDSCAPE } from './landscape';
 
-export const SERVICES: ServicesDataTypeGroup[] = [
-  DISMANTLING,
-  CONCRETE,
-  ROOFING,
-  FINISHING,
-  FACING,
-  PLUMBING,
-  WIRING,
-  FLOORING,
-  LANDSCAPE,
-];
+export const SERVICES: ServicesDataTypeGroup[] = [DISMANTLING, FLOORING, LANDSCAPE];
