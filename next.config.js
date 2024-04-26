720d9585a9ef769d5351350c456622b7223d6ae9
KohnoA <san.kohno@yandex.ru>
2024-04-26 21:42:33 +0300

    refactor: removed comments in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'export',
-  // images: {
-  //   loader: 'imgix',
-  //   path: '/',
-  // },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
